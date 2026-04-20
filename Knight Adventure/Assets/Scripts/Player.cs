ba421029094dabfd3c92394dcbc774b921d2bd68
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-04-20 12:48:05 +0530

    PLayer jump and flip

--- Knight Adventure/Assets/Scripts/Player.cs
+++ Knight Adventure/Assets/Scripts/Player.cs
@@ -5,6 +5,13 @@ public class Player : MonoBehaviour
     [Header("Movement Settings")]
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private int facingDirection = 1;
+
+    [Header("Ground Check Settings")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private bool isGrounded;
 
     [Header("References")]
     [SerializeField] private Rigidbody2D rb;
@@ -15,6 +22,13 @@ public class Player : MonoBehaviour
     private void Update()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
+
+        Jump();
+
+        if(moveInput > 0 && transform.localScale.x < 0 || moveInput < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
     }
 
     void FixedUpdate()
@@ -25,5 +39,21 @@ public class Player : MonoBehaviour
     private void Move()
     {
         rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+    }
+
+    void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
